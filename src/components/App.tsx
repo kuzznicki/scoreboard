5124a791a6a3e2908f77698639a180517e50789d
kuzznicki <kuznicki@protonmail.com>
2022-12-20 23:46:29 +0100

    add bootstrap & sass

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@/styles/App.scss';
+
+function App() {
+    return (
+        <div className="App">
+            <Button variant="primary">Test</Button>
+        </div>
+    )
+}
+
+export default App
